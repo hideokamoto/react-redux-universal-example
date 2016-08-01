b68a2acde5039d8b80e58ff71381d4ea5f7a048f
hideokamoto <hide.okamoto@digitalcube.jp>
2016-08-01 09:51:34 +0900

    request api

--- common/api/counter.js
+++ common/api/counter.js
@@ -1,11 +1,31 @@
+import fetch from 'isomorphic-fetch';
+
 function getRandomInt(min,max) {
 	return Math.floor( Math.random() * (max - min )) + min;
 }
+function fetchData( api ) {
+	return fetch(api);
+}
 
 export function fetchCounter(callback) {
-	setTimeout( () => {
-		callback(getRandomInt(1,100));
-	}, 500 );
+
+	fetchData('http://localhost:3000/api/counts')
+	  .then( res => {
+		  if ( res.status >= 400 ) {
+			  console.log(res);
+			  throw new Error('Bad request');
+		  }
+		  return res.json();
+	  })
+	  .then( data => {
+		  console.log(data[0].value);
+		  callback(data[0].value);
+	  })
+	  .catch( error => {
+		  console.log(error);
+		  throw error;
+	  })
+
 
 	// In the case of a real world API call,
 	// you'll normally run into a Promise like this:
