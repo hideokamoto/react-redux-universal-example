312a3f65465542401380e8933e75573a6c06bccc
hideokamoto <hide.okamoto@digitalcube.jp>
2016-08-03 08:29:15 +0900

    fix client bug

--- client/index.js
+++ client/index.js
@@ -2,7 +2,8 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, hashHistory, match } from 'react-router';
+import { Router, hashHistory, match, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 
 import configureStore from '../common/store/configureStore';
 import routes from '../server/route';
@@ -10,6 +11,7 @@ import routes from '../server/route';
 const preloadState = window.__PRELOADED_STATE__;
 const store = configureStore(preloadState);
 const rootElement = document.getElementById('app');
+var history = syncHistoryWithStore(browserHistory, store);
 
 match({history, routes}, (error,redirectLocation,renderProps) => {
 	render(
